599bd5cef70e3515333b55c0d24e604fcdb2807d
Elijah Watson <ElijahLWatson@Gmail.com>
2019-09-12 18:32:02 -0400

    Add responsive image functionality in webpack

--- webpack.common.js
+++ webpack.common.js
@@ -10,16 +10,40 @@ module.exports = {
 		rules: [
 			{
 				test: /\.html$/,
-				use: ['html-loader']
+				use: { 
+					loader: 'html-loader', 
+					options: { interpolate: true } 
+				}
 			},
 			{
 				test: /\.(jpe?g|png|gif|svg)$/i,
+				include: [
+					path.resolve(__dirname, 'resources/css/images'),
+				],
 				use: [
 					{
 						loader: 'file-loader',
 						options: {
 							outputPath: 'images',
 							name: '[name].[hash].[ext]',
+						}
+					}
+				]
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				include: [
+					path.resolve(__dirname, 'resources/images'),
+				],
+				use: [
+					{
+						loader: 'responsive-loader',
+						options: {
+							outputPath: 'images',
+							sizes: [300, 600, 1200, 2000],
+							placeholder: true,
+							placeholderSize: 50,
+							name: '[name]-[width].[hash].[ext]'
 						}
 					}
 				]
