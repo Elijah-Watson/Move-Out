a48152fb24c64e98fb3da210efdcf222b1874e6f
Elijah Watson <ElijahLWatson@Gmail.com>
2019-09-20 01:13:37 -0400

    Add guides page placeholders and styles

--- resources/js/guides.js
+++ resources/js/guides.js
@@ -1,7 +1,12 @@
 import '../css/main.css';
 import { StickyNavBar } from './components/sticky-nav-bar';
+import { ImageCover } from './components/image-cover';
 
 (function onLoad() {
 	let navBar = new StickyNavBar(document.querySelector('.site-nav'));
 	navBar.init();
+
+	let imageCoverElements = [...document.querySelectorAll('.image-cover')];
+	let imageCoverObjects = imageCoverElements.map(imageCoverElement => new ImageCover(imageCoverElement));
+	imageCoverObjects.forEach(imageCoverObject => imageCoverObject.init());
 })();
